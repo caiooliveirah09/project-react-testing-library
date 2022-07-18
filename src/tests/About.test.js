05bbdbdbd61d9623c5bf4587dbeadadd763d473a
caiooliveirah09 <leelros@hotmail.com>
2022-07-18 18:27:51 -0300

    i solved requirement 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,12 +5,12 @@ import renderWithRouter from './renderWithRouter';
 const { screen } = require('@testing-library/react');
 
 describe('About tests', () => {
-  test('1-test if the page contains an h2 heading with the text About pokédex;', () => {
+  test('1-test if the page contains an h2 heading with the text About pokédex', () => {
     renderWithRouter(<About />);
     const heading = screen.getByRole('heading', { name: 'About Pokédex' });
     expect(heading).toBeInTheDocument();
   });
-  test('2-test if the page contains two paragraphs of text about the Pokédex;;', () => {
+  test('2-test if the page contains two paragraphs of text about the Pokédex', () => {
     renderWithRouter(<About />);
     const AboutText1 = screen.getByText(/This application simulates a Pokédex/);
     const AboutText2 = screen.getByText(/One can filter Pokémons by type/i);
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'test',
+  },
+];
+
+describe('FavoritePokemons tests', () => {
+  test('1-test if the msg "No favorite pokemon found is displayed on the screen"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const FavoritePokemonsText = screen.getByText('No favorite pokemon found');
+    expect(FavoritePokemonsText).toBeInTheDocument();
+  });
+
+  test('2-test if all favorite Pokemon cards are displayed', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const test = screen.getByText('Pikachu');
+    expect(test).toBeInTheDocument();
+  });
+});
