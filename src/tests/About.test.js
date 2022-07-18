139cccde09d5c8433a5969a137cf482682e3fab8
caiooliveirah09 <leelros@hotmail.com>
2022-07-18 18:04:48 -0300

    i solved requirement 1, 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('About tests', () => {
+  test('1-test if the page contains an h2 heading with the text About pokédex;', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+  });
+  test('2-test if the page contains two paragraphs of text about the Pokédex;;', () => {
+    renderWithRouter(<About />);
+    const AboutText1 = screen.getByText(/This application simulates a Pokédex/);
+    const AboutText2 = screen.getByText(/One can filter Pokémons by type/i);
+    expect(AboutText1).toBeInTheDocument();
+    expect(AboutText2).toBeInTheDocument();
+  });
+  test('3-', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByAltText('Pokédex');
+    expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -21,4 +21,18 @@ describe('App tests', () => {
     userEvent.click(home);
     expect(history.location.pathname).toBe('/');
   });
+  test('3-test if the app is redirected to the favorite pokemons page', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoritePokemons);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+  test('4-test if the app using an unknown url is redirected to not found page', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/unknown-url');
+    const notFoundText = screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
 });
