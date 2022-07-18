9b79c1bd1785266832120959602190c3787203f4
caiooliveirah09 <leelros@hotmail.com>
2022-07-18 16:46:39 -0300

    test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+const { screen } = require('@testing-library/react');
+
+describe('App tests', () => {
+  test('1-test if nav links appear on screen', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+  test('2-test if the app is redirected to the home page', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(home);
+    expect(history.location.pathname).toBe('/');
+  });
+});
